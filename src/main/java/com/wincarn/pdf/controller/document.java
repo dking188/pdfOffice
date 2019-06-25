b56510093e73de609c2057279bd84a4108df867c
dking <626600175@qq.com>
2019-06-25 13:57:24 +0800

    first commit

---
+++ src/main/java/com/wincarn/pdf/controller/document.java
@@ -0,0 +1,22 @@
+package com.wincarn.pdf.controller;
+
+import javax.annotation.Resource;
+
+/**
+ * @ProjectName:
+ * @Package:
+ * @ClassName:
+ * @Author: Dking
+ * @Email: dking@188.com
+ * @Description:
+ * @Date: 2019-06-25 13:52
+ * @Version: 1.0
+ */
+public class document {
+
+
+    @Resource
+    private DocumentConverter documentConverter;
+
+
+}
