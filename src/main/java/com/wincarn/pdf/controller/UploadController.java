44cc3c50ab6cb507c5a878b774eae134a68cab33
dking <626600175@qq.com>
2019-06-25 17:06:18 +0800

    upload method

---
+++ src/main/java/com/wincarn/pdf/controller/UploadController.java
@@ -0,0 +1,118 @@
+package com.wincarn.pdf.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+
+/**
+ * @ProjectName:
+ * @Package:
+ * @ClassName:
+ * @Author: Dking
+ * @Email: dking@188.com
+ * @Description:
+ * @Date: 2019-06-25 16:27
+ * @Version: 1.0
+ */
+@Controller
+public class UploadController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadController.class);
+
+    @GetMapping("/upload")
+    public String upload() {
+        return "upload";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "/Users/energy/workspace/pdf/temp/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            LOGGER.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            LOGGER.error(e.toString(), e);
+        }
+        return "上传失败！";
+    }
+
+    @GetMapping("/multiUpload")
+    public String multiUpload() {
+        return "multiUpload";
+    }
+
+    @PostMapping("/multiUpload")
+    @ResponseBody
+    public String multiUpload(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        String filePath = "/Users/energy/workspace/pdf/temp/";
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            if (file.isEmpty()) {
+                return "上传第" + (i++) + "个文件失败";
+            }
+            String fileName = file.getOriginalFilename();
+
+            File dest = new File(filePath + fileName);
+            try {
+                file.transferTo(dest);
+                LOGGER.info("第" + (i + 1) + "个文件上传成功");
+            } catch (IOException e) {
+                LOGGER.error(e.toString(), e);
+                return "上传第" + (i++) + "个文件失败";
+            }
+        }
+        return "上传成功";
+
+    }
+
+    //获取根目录 /tmp
+    private static String getResourceBasePath() {
+        // 获取跟目录
+        File path = null;
+        try {
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            // nothing to do
+        }
+        if (path == null || !path.exists()) {
+            path = new File("");
+        }
+
+        String pathStr = path.getAbsolutePath();
+        // 如果是在eclipse中运行，则和target同级目录,如果是jar部署到服务器，则默认和jar包同级
+        pathStr = pathStr.replace("\\target\\classes", "");
+
+        return pathStr;
+
+
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/list.jsp
@@ -0,0 +1,30 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>文件列表</title>
+</head>
+<body>
+
+<table style="border:1px solid gray;" id="table" class="sort-table table table-bordered table-hover" data-async="false">
+    <thead>
+    <tr>
+        <th>文件名</th>
+        <th>查看</th>
+
+    </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td>预览</td>
+        </tr>
+
+
+    </tbody>
+</table>
+
+</body>
+</html>
